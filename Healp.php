2debcd271ac993ec5605b59e6ee707032b9aa723
Higino Figueiredo Sessa <higinosessa370@gmail.com>
2026-04-09 13:27:05 +0300

    NN

--- Healp.php
+++ Healp.php
@@ -1,9 +1,30 @@
 <?php
 
-echo "Esta pagina e para nos ajudar";
 
-function textoApp(string $texto)
+
+function textoApp(string $texto="Esta pagina e para nos ajudar")
 {
 return $texto;
+}
+
+///////////////////////////////////
+
+function Analizado($saudacao= "bom dia"):string
+{
+$hora = 14;
+
+if($hora >= 6 && $hora <= 12)
+{
+    $saudacao = 'Bom dia';
+}
+
+if($hora >=13 && $hora <=18)
+{
+    $saudacao = 'Boa Tarde';
+}
+
+
+return $saudacao;
+
 }
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-require_once 'connecta/processa.php';
+require_once 'processa/connecta.php';
 include_once 'Healp.php';
-
-
-echo textoApp(' Estamos todos unidos para mesmas causass');
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo textoApp();?></h1>
+    <hr>
+     <h1><?php echo Analizado();?></h1>
+
+</body>
+</html>
